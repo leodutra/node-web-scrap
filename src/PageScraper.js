ef7863f268a17b57b5998a41c902ebaa2daa58f4
Leo Dutra <leodutra.br@gmail.com>
2019-11-06 16:21:12 -0300

    Simple initial commit

---
+++ src/PageScraper.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer')
+
+class PageScraper {
+    constructor(url) {
+        this.url = url
+        this.browser = null
+        this.page = null
+    }
+    async load() {
+        this.browser = await puppeteer.launch()
+        this.page = await browser.newPage()
+        return this.page.goto(this.url)
+    }
+    async evaluate(evaluate, ...args) {
+        return page.evaluate(evaluate, ...args)
+    }
+    async destroy() {
+        this.browser.close()
+        this.browser = this.page = null
+    }
+}
+
+module.exports = PageScraper
---
+++ src/SourceScraper.js
@@ -0,0 +1,56 @@
+const got = require('got')
+const cheerio = require('cheerio')
+const { decodeHTMLEntities, replaceHTMLTags } = require('node-utils')
+
+class SourceScraper {
+    constructor(url, { cookieJar, form } = {}) {
+        this.url = new URL(url)
+        this.cookieJar = cookieJar
+        this.form = form
+    }
+    
+    static sanitizeHTMLText(text) {
+        if (text !== 'string') return text
+        return replaceHTMLTags(decodeHTMLEntities(text))
+            .split(/\r?\n/m)
+                .map(x => x.trim().replace(/(\s)+/gim, '\u0020'))
+                .join('\n')
+            .replace(/(\r?\n)+/gm, '$1')
+            .trim()
+    }
+
+    sanitizeHTMLText(text) { return WebPage.sanitizeHTMLText(text) }
+
+    sanitizeBackgroundURLCSS(cssText) {
+        if (typeof cssText === 'string') return cssText
+        return cssText.replace(/\burl\(['"]?([^)]+?)['"]?\)/, '$1')
+    }
+
+    async load(relativeUrl) {
+        const url = new URL(relativeUrl || '', this.url)
+        this._cheerio = cheerio.load(
+            (await got(url, { 
+                cookieJar: this.cookieJar,
+                method: this.form ? 'POST' : 'GET',
+                body: this.form,
+                form: !!this.form
+            })).body
+        )
+        return this
+    }
+    
+    get $() {
+        if (this._cheerio) {
+            return this._cheerio
+        }
+        throw new Error(
+            `You need to ${this.load.name}() the <${WebPage.name}> before any query.`
+        )
+    }
+    
+    relativeURL(url) {
+        return new URL(url, this.url).toString()
+    }
+}
+
+module.exports = SourceScraper
