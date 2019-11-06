1ca177f6aabc7579f46f70a359c9f4fe6e7eb08b
Leo Dutra <leodutra.br@gmail.com>
2019-11-06 18:59:01 -0300

    Implements PageScraper.scrap()

--- src/PageScraper.js
+++ src/PageScraper.js
@@ -4,18 +4,28 @@ class PageScraper {
         this.browser = browser
         this.page = null
     }
-    async load(onPageLoad) {
+    async load(opts = {}) {
+        const { onPageLoad } = opts
         this.page = await this.browser.newPage()
-        if (typeof onPageLoad == 'function') {
+        if (onPageLoad) {
             this.page.on('load', onPageLoad)
         }
-        return this.page.goto(this.url)
+        return this.page.goto(this.url, opts)
     }
     async evaluate(evaluate, ...args) {
         if (!this.page) {
             throw new Error(`Please ${this.load.name}() ${PageScraper.name} before calling ${this.evaluate.name}().`)
         }
         return this.page.evaluate(evaluate, ...args)
+    }
+    async scrap(waitForSelector, evaluate, ...args) {
+        await this.load()
+        if (waitForSelector) {
+            await this.page.waitForSelector(waitForSelector)
+        }
+        const result = await this.evaluate(evaluate, ...args)
+        await this.unload()
+        return result
     }
     async unload() {
         if (this.page) {
