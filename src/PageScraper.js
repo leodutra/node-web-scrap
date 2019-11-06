2d20d5e050fbc13a9c78ba84f8e9b45cf452dd7f
Leo Dutra <leodutra.br@gmail.com>
2019-11-06 17:03:29 -0300

    Simplify PageScraper

--- src/PageScraper.js
+++ src/PageScraper.js
@@ -3,16 +3,12 @@ const puppeteer = require('puppeteer')
 class PageScraper {
     constructor(url) {
         this.url = url
-        this.browser = null
-        this.page = null
-    }
-    async load() {
         this.browser = await puppeteer.launch()
         this.page = await this.browser.newPage()
-        this.page.on('load', this.onPageLoad)
+    }
+    async load() {
         return this.page.goto(this.url)
     }
-    async onPageLoad() {}
     async evaluate(evaluate, ...args) {
         return this.page.evaluate(evaluate, ...args)
     }
