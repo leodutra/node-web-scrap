1421ef596af959e9a57a85d7d4f2893df1b5c883
Leo Dutra <leodutra.br@gmail.com>
2019-11-06 17:07:45 -0300

    Changes PageScraper structure

--- src/PageScraper.js
+++ src/PageScraper.js
@@ -3,10 +3,15 @@ const puppeteer = require('puppeteer')
 class PageScraper {
     constructor(url) {
         this.url = url
+        this.browser = null
+        this.page = null
+    }
+    async load(onPageLoad) {
         this.browser = await puppeteer.launch()
         this.page = await this.browser.newPage()
-    }
-    async load() {
+        if (typeof onPageLoad == 'function') {
+            this.page.on('load', onPageLoad)
+        }
         return this.page.goto(this.url)
     }
     async evaluate(evaluate, ...args) {
